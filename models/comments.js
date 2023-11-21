7ba3d26716fd5f53653f003f634a982ad4b23180
DuvanJR <duvanjaimes14@gmail.com>
2023-11-21 16:00:21 -0500

    cambios en los datos de comments

--- models/comments.js
+++ models/comments.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const commentsSchema = new mongoose.Schema({
     like: { type: Boolean, default: 0 },
-    dislike: { type: Boolean, default: 0 },
     fecha: { type: Date, default: Date.now },
     comentario: { type: String, required: true },
     universityName: { type: String, required: true },
