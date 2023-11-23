2d663bb8d579cdf10d870028f4b0495dedc58e4b
DuvanJR <duvanjaimes14@gmail.com>
2023-11-23 09:55:40 -0500

    se agrego el metodo para obtener todos los comentarios

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -5,19 +5,33 @@ const bodyParser = require('body-parser');
 const Comment = require('../models/comments');
 router.use(bodyParser.json());
 
-// Ruta para obtener todos los datos de University
+// Ruta para crear comentarios
 router.post('/comments', async (req, res) => {
   try {
     const { like, fecha, comentario, universityName,idUniversity, usuario } = req.body;
     const newComment = new Comment({ like, fecha, comentario, universityName, idUniversity, usuario});
+    console.log(newComment)
     await newComment.save();
     res.status(201).json({ message: 'Comentario creado exitosamente', comment: newComment });
+    console.log(newComment)
   } catch (err) {
     res.status(400).json({ message: 'Error al crear el comentario', error: err.message });
   }
 });
 
+
 // Ruta para obtener todos los comentarios
+router.get('/comments', async (req, res) => {
+  try {
+    const comments = await Comment.find({});
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al obtener los comentarios', error: err.message });
+  }
+});
+
+
+// Ruta para obtener los comentarios por id
 router.get('/comments/:idUniversity', async (req, res) => {
   try {
     const idUniversity = req.params.idUniversity;
@@ -27,6 +41,8 @@ router.get('/comments/:idUniversity', async (req, res) => {
     res.status(500).json({ message: 'Error al obtener los comentarios', error: err.message });
   }
 });
+
+
 
 // Ruta para el estado del like /true - false
 router.post('/comments/:usuario/:like', async (req, res) => {
